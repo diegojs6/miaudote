f702e9b36e6c751bd36d74373669a433b5d49491
Rodrigo Amancio <rodrigoa.work@gmail.com>
2022-01-16 12:52:15 -0300

    resolved problems

--- lib/core/api/api_interceptor.dart
+++ lib/core/api/api_interceptor.dart
@@ -1,17 +1,12 @@
 import 'package:http/http.dart';
-import 'package:http_interceptor/http/http.dart';
-import 'package:http_interceptor/http/interceptor_contract.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 
 abstract class IHttpClient {
   Future<Response> get(String? endpoint, {Map<String, dynamic>? headers});
-  Future<Response> post(String? endpoint, String body,
-      {Map<String, String>? headers});
-  Future<Response> patch(String? endpoint, String body,
-      {Map<String, String>? headers});
-  Future<Response> put(String? endpoint, String body,
-      {Map<String, String>? headers});
+  Future<Response> post(String? endpoint, String body, {Map<String, String>? headers});
+  Future<Response> patch(String? endpoint, String body, {Map<String, String>? headers});
+  Future<Response> put(String? endpoint, String body, {Map<String, String>? headers});
   Future<Response> delete(String? endpoint, {Map<String, String>? headers});
 }
 
@@ -42,75 +37,54 @@ class HttpClient extends IHttpClient implements InterceptorContract {
     return data;
   }
 
-   @override
+  @override
   Future<Response> get(String? endpoint, {Map<String, dynamic>? headers}) async {
-   final response = await _client.get(
-     endpoint!.toUri(),
-     headers: {
-        'X-Parse-Application-Id': '1OKzIuoVO9qiDqRblLdzej0EozjBg8Un7hVQ8SL5',
-        'X-Parse-REST-API-Key': 'CtiY719Ne7ijJyTqHI0c4Wi3217ROuHketaqyubS',
-        'Content-Type': 'application/json'
-     }
-   );
+    final response = await _client.get(endpoint!.toUri(), headers: {
+      'X-Parse-Application-Id': '1OKzIuoVO9qiDqRblLdzej0EozjBg8Un7hVQ8SL5',
+      'X-Parse-REST-API-Key': 'CtiY719Ne7ijJyTqHI0c4Wi3217ROuHketaqyubS',
+      'Content-Type': 'application/json'
+    });
 
-   return response;
-  }
-
-    @override
-  Future<Response> post(String? endpoint, String body,
-      {Map<String, String>? headers}) async{
-    final response = await _client.post(
-      endpoint!.toUri(),
-      body: body,
-       headers: {
-        'X-Parse-Application-Id': '1OKzIuoVO9qiDqRblLdzej0EozjBg8Un7hVQ8SL5',
-        'X-Parse-REST-API-Key': 'CtiY719Ne7ijJyTqHI0c4Wi3217ROuHketaqyubS',
-        'Content-Type': 'application/json'
-     }
-    );
     return response;
   }
 
   @override
-  Future<Response> put(String? endpoint, String body,
-      {Map<String, String>? headers}) async{
-     final response = await _client.post(
-      endpoint!.toUri(),
-      body: body,
-       headers: {
-        'X-Parse-Application-Id': '1OKzIuoVO9qiDqRblLdzej0EozjBg8Un7hVQ8SL5',
-        'X-Parse-REST-API-Key': 'CtiY719Ne7ijJyTqHI0c4Wi3217ROuHketaqyubS',
-        'Content-Type': 'application/json'
-     }
-    );
-    return response;
-  }
-
-   @override
-  Future<Response> patch(String? endpoint, String body,
-      {Map<String, String>? headers})async {
-     final response = await _client.post(
-      endpoint!.toUri(),
-      body: body,
-       headers: {
-        'X-Parse-Application-Id': '1OKzIuoVO9qiDqRblLdzej0EozjBg8Un7hVQ8SL5',
-        'X-Parse-REST-API-Key': 'CtiY719Ne7ijJyTqHI0c4Wi3217ROuHketaqyubS',
-        'Content-Type': 'application/json'
-     }
-    );
+  Future<Response> post(String? endpoint, String body, {Map<String, String>? headers}) async {
+    final response = await _client.post(endpoint!.toUri(), body: body, headers: {
+      'X-Parse-Application-Id': '1OKzIuoVO9qiDqRblLdzej0EozjBg8Un7hVQ8SL5',
+      'X-Parse-REST-API-Key': 'CtiY719Ne7ijJyTqHI0c4Wi3217ROuHketaqyubS',
+      'Content-Type': 'application/json'
+    });
     return response;
   }
 
   @override
-  Future<Response> delete(String? endpoint, {Map<String, String>? headers}) async{
-     final response = await _client.post(
-      endpoint!.toUri(),
-       headers: {
-        'X-Parse-Application-Id': '1OKzIuoVO9qiDqRblLdzej0EozjBg8Un7hVQ8SL5',
-        'X-Parse-REST-API-Key': 'CtiY719Ne7ijJyTqHI0c4Wi3217ROuHketaqyubS',
-        'Content-Type': 'application/json'
-     }
-    );
+  Future<Response> put(String? endpoint, String body, {Map<String, String>? headers}) async {
+    final response = await _client.post(endpoint!.toUri(), body: body, headers: {
+      'X-Parse-Application-Id': '1OKzIuoVO9qiDqRblLdzej0EozjBg8Un7hVQ8SL5',
+      'X-Parse-REST-API-Key': 'CtiY719Ne7ijJyTqHI0c4Wi3217ROuHketaqyubS',
+      'Content-Type': 'application/json'
+    });
+    return response;
+  }
+
+  @override
+  Future<Response> patch(String? endpoint, String body, {Map<String, String>? headers}) async {
+    final response = await _client.post(endpoint!.toUri(), body: body, headers: {
+      'X-Parse-Application-Id': '1OKzIuoVO9qiDqRblLdzej0EozjBg8Un7hVQ8SL5',
+      'X-Parse-REST-API-Key': 'CtiY719Ne7ijJyTqHI0c4Wi3217ROuHketaqyubS',
+      'Content-Type': 'application/json'
+    });
+    return response;
+  }
+
+  @override
+  Future<Response> delete(String? endpoint, {Map<String, String>? headers}) async {
+    final response = await _client.post(endpoint!.toUri(), headers: {
+      'X-Parse-Application-Id': '1OKzIuoVO9qiDqRblLdzej0EozjBg8Un7hVQ8SL5',
+      'X-Parse-REST-API-Key': 'CtiY719Ne7ijJyTqHI0c4Wi3217ROuHketaqyubS',
+      'Content-Type': 'application/json'
+    });
     return response;
   }
 }
